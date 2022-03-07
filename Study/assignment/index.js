b1571d6d38a845f4bb54ee448f52d4e7d7d96da9
chosey0 <csq1404@gmail.com>
2022-03-07 13:07:01 +0900

    개강 2주차

--- Study/assignment/index.js
+++ Study/assignment/index.js
@@ -1,5 +1,7 @@
 let product = document.querySelector(".product");
+const expand_header = document.querySelector("header");
+const header_bottom = document.getElementById("expand_header");
 
-product.addEventListener("mouseover", () => {
-  HTMLDivElement;
+expand_header.addEventListener("mouseover", () => {
+  header_bottom.classList.toggle("show");
 });
