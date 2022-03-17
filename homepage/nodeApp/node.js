794998687016f047dfe9cb33ad24d479bf4a091c
chosey0 <csq1404@gmail.com>
2022-03-18 00:39:21 +0900

    학교 수업 과제

---
+++ homepage/nodeApp/node.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const app = express();
+const path = require("path");
+
+// 경로 정의
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/template.html");
+});
+
+//  포트 번호 3000번이 반환되어 서버가 실행되고
+app.listen(3000, function () {
+  //  연결 확인 콘솔이 콜백됨
+  console.log("Example app listening on port 3000!");
+});
+
+// http.createServer(app).listen(80, () => {
+//   console.log("Example app listening on port 3000!");
+// });
+
+// https.createServer(app).listen(443, () => {
+//   console.log("Example app listening on port 3000!");
+// });
---
+++ homepage/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./**/*.{html,js}",
+    "./myweb/src/*.html",
+    "./main/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
