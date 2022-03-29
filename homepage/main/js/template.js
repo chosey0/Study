e786804fb0c2862e3455b3867dcece0f8f2b4b71
chosey0 <csq1404@gmail.com>
2022-03-29 11:15:07 +0900

    과제

---
+++ homepage/main/js/template.js
@@ -0,0 +1,37 @@
+$("document").ready(function () {
+  var trigger = $("#menu-icon"),
+    isClosed = true;
+  var footer = $("#footer");
+  var side_menu = $("#side-menu");
+  var main = $("main");
+  trigger.click(function () {
+    menuTime();
+    displayToggle();
+  });
+
+  function menuTime() {
+    if (isClosed == true) {
+      trigger.removeClass("is-open");
+      trigger.addClass("is-closed");
+      isClosed = false;
+    } else {
+      trigger.removeClass("is-closed");
+      trigger.addClass("is-open");
+      isClosed = true;
+    }
+  }
+
+  function displayToggle() {
+    if (isClosed == true) {
+      main.hide();
+      side_menu.show();
+      footer.addClass("hidden-sm");
+      footer.addClass("hidden-xs");
+    } else {
+      main.show();
+      side_menu.hide();
+      footer.removeClass("hidden-sm");
+      footer.removeClass("hidden-xs");
+    }
+  }
+});
